9e55e1530d6b5da18c1efcce5287ea72b7898ee9
nickfryar <nrf160030@utdallas.edu>
2017-03-30 20:52:48 -0500

    Win checking algorithm (partial)
    Horizontal and vertical win checking (plus some code organization)

--- connect4.asm
+++ connect4.asm
@@ -13,23 +13,29 @@
 ##################################################
 
 		.data
-board:		.space	196	# 4 bytes per space (0, 1, 2, and 3 for empty, p1, p2, and binding respectively )
-title:		.asciiz	"\n===== Connect-Four =====\n"
-options:	.asciiz "\n1) Player v. AI\n2) Player v. Player\n3) Instructions\n4) Exit\n"
-prompt:		.asciiz	" : "
-piece:		.asciiz "| "
-row_end:	.asciiz "|\n"
-turn:		.asciiz "\nChoose column to drop piece: "
+# Array and variables
+board:		.space	196	# 4 bytes per space (0, 1, 2, and 3 for empty, p1, p2, and binding respectively)
+diff_setting:	.word	0
+# Strings
 ai_turn:	.asciiz	"\nAI playing...\n"
-p1_turn:	.asciiz "\nPlayer 1 - "
-p2_turn:	.asciiz	"\nPlayer 2 - "
+ai_win:		.asciiz	"\nYou lose...\n"
 close:		.asciiz	"\nThanks for playing!"
+difficulty:	.asciiz	"\nChoose a difficulty.\n(1-easy, 2-medium, 3-hard): "
+error:		.asciiz	"\nAn error occurred. :(\n"
 instruct1:	.asciiz "\nDrop markers into columns from the top of the board and\n"
 instruct2:	.asciiz "try to get four pieces in a row (horizontal, vertical, or diagonal).\n"
 instruct3:	.asciiz "Note: markers can only be placed in columns with empty space.\n"
-win_message:	.asciiz	"SOMEONE WON!"
-difficulty:	.asciiz	"Please choose level of difficulty: 1 for Easy, 2 for Medium, 3 for Hard and 4 for exit."
-wrong_input:	.asciiz "\n\nWrong data was entered. Please try again."
+options:	.asciiz "\n1) Player v. AI\n2) Player v. Player\n3) Instructions\n4) Exit\n"
+p1_turn:	.asciiz "\nPlayer 1 - "
+p2_turn:	.asciiz	"\nPlayer 2 - "
+piece:		.asciiz "| "
+player_win:	.asciiz	"\nYou win!\n"
+player1_win:	.asciiz	"\nPlayer 1 wins!\n"
+player2_win:	.asciiz	"\nPlayer 2 wins!\n"
+prompt:		.asciiz	" : "
+row_end:	.asciiz "|\n"
+title:		.asciiz	"\n===== Connect-Four =====\n"
+turn:		.asciiz "\nChoose column to drop piece: "
 		
 		.text
 # Entry point
@@ -56,15 +62,12 @@ M_pro:
 		beq	$v0, 3, M_ins		# Branch to instructions option
 		beq	$v0, 4, Exit		# Branch to Exit
 		j	M_pro			# Prompt user again if input invalid
-# Player vs AI
 M_pve:
 		jal	PvE			# Jump and link to PvE
 		j	Menu			# Jump to menu
-# Player vs Player
 M_pvp:
 		jal	PvP			# Jump and link to PvP
 		j	Menu			# Jump to Menu
-# Display instructions
 M_ins:
 		jal	Ins			# Jump and link to Ins
 		j	Menu			# Jump to Menu
@@ -76,88 +79,53 @@ M_ins:
 #	then prompts the user to start a new game, return the menu,
 #	or exit the program.
 PvE:
-		addi	$sp, $sp, -4		# Get space for $ra on the stack
+		subi	$sp, $sp, 4		# Get space for $ra on the stack
 		sw	$ra, 0($sp)		# Store $ra to the stack
-		
+		jal	PvE_dPrompt
 		jal	MakeBoard		# Jump and link to MakeBoard (zero the board)
 		jal	DrawBoard		# Jump and link to DrawBoard (draw the board)
-		
-ask_difficulty:	
-		# Ask user for AI Difficulty
-		la	$a0, difficulty		# load address of the string for asking difficulty
-		li	$v0, 4			# instruction for print string
-		syscall				# execute
-		
-		# Read input from user for difficulty
-		li	$v0, 5			# instruction for read integer
-		syscall				# execute
-		move	$s1, $v0		# store this into one of the other registers so that $v0 may be used for other things
-		
-		# Checking to see if user wants to exit
-		li	$t0, 4
-#		beq	$s1, $t0, exit_PvE
-		
-		# Input validation
-		ble	$s1, $0, validate	# if input <= 0
-		bgt	$s1, $t0, validate	# if input > 4
-		
-
+		j	PvE_loop
+PvE_dPrompt:
+		li	$v0, 4
+		la	$a0, difficulty		# Prompt the user to choose a difficulty setting
+		syscall
+		li	$v0, 5
+		syscall				# Get input from user
+		la	$a0, diff_setting
+		sw	$v0, 0($a0)
+		jr	$ra
 PvE_loop:
+		# Loop between turns for player and AI
 		li	$a0, 1
 		jal	PTurn			# Jump and link to PTurn (player turn)
-		#jal	CheckWin
+		beq	$v0, 1, PvE_win		# If win, branch to PvE_win
 		li	$a0, 2
-		jal	AI_Time			# Jump and link to AITurn (AI turn)
-		#jal	CheckWin
-			j PvE_loop			# TEMP
+		jal	AITurn			# Jump and link to AITurn (AI turn)
+		beq	$v0, 1, PvE_win		# If win, branch to PvE_win
+		j PvE_loop			# Else, loop again
+PvE_win:
+		# Branch to appropriate win-subroutine
+		beq	$a0, 1, PvE_win1	# If ($a0 == 1), branch to PvE_win1
+		beq	$a0, 2, PvE_win2	# If ($a0 == 2), branch to PvE_win2
+		li	$v0, 4
+		la	$a0, error
+		syscall				# Else, display error message
+		j	PvE_back
+PvE_win1:
+		li	$v0, 4
+		la	$a0, player_win
+		syscall				# Display player-win message
+		j	PvE_back
+PvE_win2:
+		li	$v0, 4
+		la	$a0, ai_win
+		syscall				# Display AI-win message
+		j	PvE_back
+PvE_back:
+		# Reload $ra and jump back to caller
 		lw	$ra, 0($sp)		# Restore $ra from the stack
 		addi	$sp, $sp, 4		# Remove space for $ra from stack
 		jr	$ra			# Jump back to caller
-
-
-AI_Time:
-		addi	$sp, $sp, -4		# make space on stack pointer
-		sw	$ra, ($sp)		# store the return address in stack pointer
-		
-		# if choice == 1
-		li	$t0, 1
-		beq	$s1, $t0, easy
-		
-		# else if choice == 2
-		li	$t0, 2
-		beq	$s1, $t0, medium
-		
-		# else if choice == 3
-		li	$t0, 3
-		#beq	$s1, $t0, hard
-
-
-easy:
-		# easy is completely random
-		li	$a1, 8			# get random number between 0 and 7
-		li	$v0, 42			# instruction for get random integer
-		syscall				# execute
-		j	AITurn
-		
-medium:
-		# medium is mix of random and some strategy
-		# it will use winning strategy to determine the best spot to play
-		# then, it will use a random number between 0 and 2 to determine to play to the column on the left, right on the best column, or on the right to play
-		#jal	winning_strategy	# call winning strategy
-		
-		# pick a random integer between 0 and 2
-		li	$a1, 3			# get random number between 0 and 2
-		li	$v0, 42			# instruction for get random integer
-		syscall				# execute
-		addi	$a0, $a0, -1		# if the random integer is 1, then the amount to increment the column number would be zero
-		j	AITurn
-		
-validate:
-		la	$a0, wrong_input	# Load address of the string to tell user that wrong data entered
-		li	$v0, 4			# Instruction for print string
-		syscall				# Execute
-		
-		j	ask_difficulty		# Loop again
 
 
 # PvP component
@@ -166,24 +134,45 @@ validate:
 #	then prompts the user to start a new game, return the menu,
 #	or exit the program.
 PvP:
-		addi	$sp, $sp, -4		# Get space for $ra on the stack
+		subi	$sp, $sp, 4		# Get space for $ra on the stack
 		sw	$ra, 0($sp)		# Store $ra to the stack
 		jal	MakeBoard
 		jal	DrawBoard		# Jump and link to DrawBoard (draw the board)
 PvP_loop:
+		# Loop between turns for players
 		li	$v0, 4
 		la	$a0, p1_turn
 		syscall				# Print p1_turn
 		li	$a0, 1			# Set indicator ($a0) to 1
 		jal	PTurn			# Jump and link to PTurn (player turn) for P1
-		#jal	CheckWin
+		beq	$v0, 1, PvP_win		# If win, branch to PvP_win
 		li	$v0, 4
 		la	$a0, p2_turn
 		syscall				# Print p2_turn
 		li	$a0, 2			# Set indicator ($a0) to 2
 		jal	PTurn			# Jump and link to PTurn (player turn) for P2
-		#jal	CheckWin
-			j PvP_loop			# TEMP
+		beq	$v0, 1, PvP_win		# If win, branch to PvP_win
+		j PvP_loop
+PvP_win:
+		# Branch to appropriate win-subroutine
+		beq	$a0, 1, PvP_win1	# If ($a0 == 1), branch to PvP_win1
+		beq	$a0, 2, PvP_win2	# If ($a0 == 1), branch to PvP_win2
+		li	$v0, 4
+		la	$a0, error
+		syscall				# Else, display error message
+		j	PvP_back
+PvP_win1:
+		li	$v0, 4
+		la	$a0, player1_win
+		syscall				# Display player1-win message
+		j	PvP_back
+PvP_win2:
+		li	$v0, 4
+		la	$a0, player2_win
+		syscall				# Display player2-win message
+		j	PvP_back
+PvP_back:
+		# Reload $ra and jump back to caller
 		lw	$ra, 0($sp)		# Restore $ra from the stack
 		addi	$sp, $sp, 4		# Remove space for $ra from stack
 		jr	$ra			# Jump back to caller
@@ -214,7 +203,7 @@ MB_loop1:
 		addi	$t0, $t0, 4		# $t0 += 4
 		j	MB_loop1			# Jump to start of loop
 MB_loop2:
-		beq	$t0, 200, MB_stop	# Stop if end of board has been reached
+		beq	$t0, 196, MB_stop	# Stop if end of board has been reached
 		li	$t1, 3
 		sw	$t1, board($t0)		# Store 0 at the base address plus the offset ($t0)
 		addi	$t0, $t0, 4		# $t0 += 4
@@ -279,7 +268,7 @@ DB_draw:
 		beq	$t1, 2, DB_2		# If ($t1 == 2), branch to DB_2
 		beq	$t1, 3, DB_end
 		j	Exit
-#						TODO: handle this error (space on board isn't 0, 1, or 2)
+#	TODO: handle this error (space on board isn't 0, 1, or 2)
 DB_0:
 		# Empty space
 		li	$v0, 11
@@ -313,7 +302,7 @@ DB_end:
 		syscall
 DB_back:
 		jr	$ra
-#						TODO: (optional) return 0 in $v0 if drawn successfully, 1 if error?
+#	TODO: (optional) return 0 in $v0 if drawn successfully, 1 if error?
 		
 
 # PlayerTurn component
@@ -342,8 +331,10 @@ PTurn:
 		slti	$t0, $v0, 8
 		beq	$t0, $zero, PTurn	# Restart prompt if input > 7
 		
-#		TODO: display error messages to user for invalid input (!)
-AI_Turn:
+#	TODO: display error messages to user for invalid input (!)
+
+		
+
 		# Get first item in column and check if it's 0 (empty)
 		move	$t0, $v0		# Set $t0 to $v0 (for calculations)
 		subi	$t0, $t0, 1		# Subtract 1 from $t0 (indices start at 0)
@@ -353,7 +344,7 @@ AI_Turn:
 PT_loop:
 		addi	$t0, $t0, 28		# Move down one row in the column (add 4 * 7)
 		
-#		TODO: check if bottom of column has been reached (!)
+#	TODO: check if bottom of column has been reached (!)
 		
 		lw	$t2, board($t0)		# Load the word at the base address plus offset ($t0) 
 		beq	$t2, $zero, PT_loop	# If the space is empty, move down again
@@ -363,40 +354,108 @@ PT_up:
 PT_set:
 		# Place the marker on the board and jump back to caller
 		sw	$a0, board($t0)		# Store indicator value ($t3) at the base address plus offset ($t0)
+		subi	$sp, $sp, 12		# Get space on stack
 		
-		subi	$sp, $sp, 8
-		sw	$ra, 0($sp)
+		# Store $ra and call CheckWin
+		sw	$ra, 0($sp)		# Store $ra on stack
+		move	$a1, $t0		# Argument for CheckWin (location on board)
+		move	$v1, $a1		# Argument for AITurn (location on board)
 		jal	CheckWin
-		sw	$v0, 4($sp)
-		jal	DrawBoard
-		lw	$v0, 4($sp)
-		lw	$ra, 0($sp)
-		addi	$sp, $sp, 8
+		
+		# Store $v0 and $a0 and call DrawBoard
+		sw	$v0, 4($sp)		# Store $v0 on stack
+		sw	$a0, 8($sp)		# Store $a0 on stack
+		jal	DrawBoard		# Draw the board
+		
+		# Reload registers and jump back to caller
+		lw	$a0, 8($sp)		# Load $a0 back from the stack
+		lw	$v0, 4($sp)		# Load $v0 back from the stack
+		lw	$ra, 0($sp)		# Load $ra back from the stack
+		addi	$sp, $sp, 12		# Remove space from stack
 		jr	$ra			# Jump back
 
 		
 # AITurn component
 #	Handles turns for the AI.
 AITurn:
-		# Store #a0 (indicator) on the stack
-		move	$v0, $a0
+		
+		
+		# Get space on stack and store $ra
+		subi	$sp, $sp, 12
+		sw	$ra, 0($sp)
+		sw	$a0, 4($sp)
 		
 		li	$v0, 4
 		la	$a0, ai_turn
+		syscall				# Print AI turn message
+		
+		la	$t0, diff_setting
+		lw	$t1, 0($t0)
+		beq	$t1, 1, AI_rand
+		beq	$t1, 2, AI_med
+		beq	$t1, 3, AI_strat
+		j	AI_error
+AI_next:
+		jal	CheckWin
+		sw	$v0, 8($sp)
+		jal	DrawBoard
+		j	AI_back
+AI_med:
+		# Generate random number to decide between random or strategic turn
+		li	$v0, 42
+		li	$a1, 100
+		syscall				# Generate random integer (in $a0)
+		li	$t0, 2
+		div	$a0, $t0
+		mfhi	$t1
+		
+		beq	$t1, 0, AI_rand		# If ($a0 % 2 == 0), branch to AI_rand
+		j	AI_strat		# Else, jump to AI_strat
+AI_rand:
+		# ??????? what do here
+AI_randLoop1:
+		li	$v0, 42
+		li	$a1, 6
+		syscall				# Generate random integer (in $a0)
+		sll	$a0, $a0, 2
+		lw	$t0, board($a0)
+		bne	$t0, 0, AI_randLoop1
+		
+		move	$t1, $a0
+		lw	$a0, 4($sp)
+		
+AI_randLoop2:
+		addi	$t1, $t1, 28		# Move down one row in the column (add 4 * 7)
+		lw	$t0, board($t1)		# Load the word at the base address plus offset ($t0) 
+		beq	$t0, 0, AI_randLoop2	# If the space is empty, move down again
+AI_randSet:
+		# Move back up a space on the board if the space isn't empty or the end of the column was reached
+		subi	$t1, $t1, 28		# Else, move back up one row (sub 4 * 7)
+
+		# Store word and jump back
+		sw	$a0, board($t1)
+		move	$a1, $t1		# Move address to $a1 (for CheckWin)
+		j	AI_next
+		
+AI_strat:
+		subi	$sp, $sp, 4
+		sw	$ra, 0($sp)
+		
+#		TODO: strategy code goes here
+		
+		lw	$ra, 0($sp)
+		addi	$sp, $sp, 4
+		j	AI_next
+
+AI_error:
+		li	$v0, 4
+		la	$a0, error
 		syscall
-		j	AI_Turn
-#	TODO: implement this
-			subi $sp, $sp, 4
-			sw $ra, 0($sp)
-			#jal	CheckWin
-			#jal	DrawBoard
-			lw $ra, 0($sp)
-			addi $sp, $sp, 4
+AI_back:
+		lw	$ra, 0($sp)
+		lw	$v0, 8($sp)
+		addi	$sp, $sp, 12
 		jr	$ra
-AIRand:
-#	TODO: implement this
-AIStrat:
-#	TODO: implement this
 		
 		
 # CheckWin component
@@ -404,6 +463,108 @@ AIStrat:
 #	If a win is found, 1 is returned in $v0. Otherwise, 0 is returned in $v0.
 #	Arguments: $a0 (marker), $a1 (index)
 CheckWin:
+		subi	$sp, $sp, 4
+		sw	$ra, 0($sp)
+		
+		jal	Vertical
+		beq	$v0, 1, CW_win
+		jal	Horizontal
+		beq	$v0, 1, CW_win
+		jal	DiagL
+		beq	$v0, 1, CW_win
+		jal	DiagR
+		beq	$v0, 1, CW_win
+		
+		lw	$ra, 0($sp)
+		addi	$sp, $sp, 4
+		jr	$ra
+Vertical:
+		# Check for win vertically
+		li	$s0, 0			# Set counter to 0
+		li	$v0, 0			# Set default return value
+		move	$t0, $a1		# $t0 = $a1 (for calculations)
+V_loop1:
+		# Find uppermost matching marker in column
+		subi	$t0, $t0, 28		# Move up one row
+		lw	$t1, board($t0)
+		beq	$t1, $a0, V_loop1	# Check for matching marker
+		addi	$t0, $t0, 28		# Move back down one row
+		addi	$s0, $s0, 1		# Increment counter
+V_loop2:
+		# Loop through column and count matches
+		addi	$t0, $t0, 28		# Move down one row
+		lw	$t1, board($t0)
+		bne	$t1, $a0, V_check	# If the marker doesn't match, break loop
+		addi	$s0, $s0, 1		# Else, increment counter
+		j	V_loop2			# Loop again
+V_check:
+		# Check counter and branch accordingly
+		sge	$t0, $s0, 4		# TODO: change this to check for 4 only to recude instruction count
+		bne	$t0, 1, V_back		# If win wasn't found, branch to end
+		li	$v0, 1
+V_back:
+		jr	$ra			# Jump back to caller
+Horizontal:
+		# Check for win horizontally
+		li	$s0, 0			# Set counter to 0
+		li	$v0, 0			# Set default return value
+		move	$t0, $a1
+		li	$t1, 0
+		li	$t3, 28
+H_loop1:
+		# Find beginning of row
+		addi	$t1, $t1, 28
+		slt	$t2, $t0, $t1
+		beq	$t2, 0, H_loop1
+		subi	$t1, $t1, 28
+H_loop2:
+		# Find leftmost matching marker in row
+		beq	$t0, $t1, H_loop3	# If beginning of row reached, goto loop3
+		subi	$t0, $t0, 4		# Else, move back one space
+		lw	$t2, board($t0)
+		beq	$t2, $a0, H_loop2	# If a matching marker was found, goto beginning of loop2
+		addi	$t0, $t0, 4		# Else, move forward one space
+H_loop3:
+		# Loop through row and count matches
+		addi	$s0, $s0, 1		# Increment counter
+		addi	$t0, $t0, 4		# Move forward one space
+		
+		# Branch to H_check if next row has been reached
+		li	$t1, 28
+		div	$t0, $t1
+		mfhi	$t2
+		beq	$t2, 0, H_check
+		
+		lw	$t3, board($t0)
+		bne	$t3, $a0, H_check	# If not a match, branch to H_check
+		j	H_loop3
+H_check:
+		# Check counter and branch accordingly
+		sge	$t0, $s0, 4		# TODO: change this to check for 4 only to recude instruction count
+		bne	$t0, 1, H_back		# If win wasn't found, branch to end
+		li	$v0, 1
+H_back:
+		jr	$ra			# Jump back to caller
+DiagL:
+		# Check for win diagonally (\)
+		li	$s0, 0			# Set counter to 0
+		li	$v0, 0			# Set default return value
+		
+#	TODO: implement this
+		
+		jr	$ra
+DiagR:
+		# Check for win diagonally (/)
+		li	$s0, 0			# Set counter to 0
+		li	$v0, 0			# Set default return value
+		
+#	TODO: implement this
+		
+		jr	$ra
+CW_win:
+		# Load $ra back from the stack and jump to caller
+		lw	$ra, 0($sp)
+		addi	$sp, $sp, 4
 		jr	$ra
 	
 
